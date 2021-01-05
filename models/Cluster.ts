07528d568fe4ad0a5c19a24a31484f497754e192
iproman <itechman@yandex.ru>
2021-01-05 20:46:47 +0500

    Запуск кластера в зависимости от вида/мастера

--- models/Cluster.ts
+++ models/Cluster.ts
@@ -6,9 +6,28 @@ import { ServiceInterface } from '../interfaces/ServiceInterface';
 export class Cluster {
 
     private readonly options: ServiceInterface;
+    private isMaster: boolean;
 
-    constructor(options: ServiceInterface) {
+    constructor(options: ServiceInterface, isMaster: boolean) {
         this.options = options;
+        this.isMaster = isMaster;
+    }
+
+    /**
+     * Старт кластера
+     *
+     * todo: логика запуска дочерних процессов
+     *
+     * ответ: Без понятий, как это реализовывать,
+     * я не писал ничего на node js дальше обычной работы с heroku и
+     * написания обычного API с помощью express.
+     */
+    public async startCluster() {
+        if (this.mode) {
+            if (this.isMaster) {
+                // do smt
+            }
+        }
     }
 
     /**
