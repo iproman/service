d412c1e7b318a4f25a6c6e5765ca135cd166e7f5
iproman <itechman@yandex.ru>
2021-01-05 21:04:24 +0500

    Получает вид и тип мастера в воркере

--- models/Worker.ts
+++ models/Worker.ts
@@ -1,5 +1,27 @@
+import { ServiceInterface } from '../interfaces/ServiceInterface';
+import { ClusterInterface } from '../interfaces/ClusterInterface';
+
 /**
  * Класс для работы с Worker
  */
 export class Worker {
+
+    private options: ServiceInterface;
+    private clusterMode: ClusterInterface;
+    private isMaster: boolean;
+
+    /**
+     * @param options
+     * @param clusterMode
+     * @param isMaster
+     */
+    constructor(
+        options: ServiceInterface,
+        clusterMode: ClusterInterface,
+        isMaster
+    ) {
+        this.options = options;
+        this.clusterMode = clusterMode;
+        this.isMaster = isMaster;
+    }
 }
