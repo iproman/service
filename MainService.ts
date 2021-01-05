ca47e25a491b73573792a5b53e6aa4dc7983e53e
iproman <itechman@yandex.ru>
2021-01-05 20:41:03 +0500

    Выносим логику с помощью передачи опций

--- MainService.ts
+++ MainService.ts
@@ -1,6 +1,10 @@
+import { ServiceInterface } from './interfaces/ServiceInterface';
 import { TransportInterface } from './interfaces/TransportInterface';
 import { WorkerInterface } from './interfaces/WorkerInterface';
 import { Cluster } from './models/Cluster';
+import { Transport } from './models/Transport';
+import { Worker } from './models/Worker';
+
 
 /**
  * MainService
@@ -11,5 +15,15 @@ class MainService {
     private transport: TransportInterface;
     private worker: WorkerInterface;
 
+
+    /**
+     * @param options
+     */
+    constructor(options: ServiceInterface) {
+
+        this.cluster = new Cluster(options);
+        this.transport = new Transport(options);
+        this.worker = new Worker(options);
+    }
 }
 
