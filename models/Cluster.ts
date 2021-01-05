4cd4041f2c3d717a38e5e6222406e71fd9093da2
iproman <itechman@yandex.ru>
2021-01-05 20:43:20 +0500

    Добавил публичный геттер для получения вида кластера

--- models/Cluster.ts
+++ models/Cluster.ts
@@ -1,6 +1,20 @@
+import { ServiceInterface } from '../interfaces/ServiceInterface';
+
 /**
  * Класс для работы с Cluster
  */
 export class Cluster {
 
+    private readonly options: ServiceInterface;
+
+    constructor(options: ServiceInterface) {
+        this.options = options;
+    }
+
+    /**
+     * Вид кластера
+     */
+    public get mode() {
+        return !!this.options;
+    }
 }
